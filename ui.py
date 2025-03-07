fd4cfa3a85e71bc1fee2f192017f95cb780f13fa
Ethan Montclair <ethmontclair@gmail.com>
2025-03-07 10:13:14 -0700

    Revert "second commit"
    This reverts commit 9f4f19a7f2961c2bb9b67b538a66eedb6c67a294.

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-from email_data import SUBSCRIPTIONS, BILLING_FREQUENCIES, NOTE_INPUTS
-from button_funcs import copy_email_to_clipboard, clear_form, generate_email, update_amount_field
+from email_data import SUBSCRIPTIONS, BILLING_FREQUENCIES
+from button_funcs import copy_email_to_clipboard, clear_form, generate_email
 
 class EmailGenerator:
     """Class that models the full Email Generator UI"""
@@ -12,7 +12,7 @@ class EmailGenerator:
         self.root.resizable(True, True)
         self.root.configure(bg="#49A864")
 
-        ## User inputs
+        # User inputs
         # Frame to hold input boxes
         inputs_frame = ttk.Frame(root)
         inputs_frame.pack(pady=10)
@@ -26,24 +26,6 @@ class EmailGenerator:
         self.subscription_dropdown = ttk.Combobox(inputs_frame, values=SUBSCRIPTIONS)
         self.subscription_dropdown.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
-        ttk.Label(inputs_frame, text="Custom Note:").grid(row=2, column=2, padx=5, pady=5, sticky="e")
-        self.custom_note = ttk.Combobox(inputs_frame, values=NOTE_INPUTS)
-        self.custom_note.grid(row=2, column=3, padx=5, pady=5, sticky="w")
-
-        # Initialize amount label and amount entry
-        self.amount_label = ttk.Label(inputs_frame, text="Amount:")
-        self.amount_entry = ttk.Entry(inputs_frame)
-
-        self.amount_label.grid(row=3, column=2, padx=5, pady=5, sticky="e")
-        self.amount_entry.grid(row=3, column=3, padx=5, pady=5, sticky="w")
-
-        # Hide them initially
-        self.amount_label.grid_remove()
-        self.amount_entry.grid_remove()
-
-        # Bind the event to call update_amount_field from ui_helpers.py
-        self.custom_note.bind("<<ComboboxSelected>>", lambda *_: update_amount_field(self))
-
         ttk.Label(inputs_frame, text="Next Service Date (MM/DD/YYYY):").grid(row=2, column=0, padx=5, pady=5, sticky="e")
         self.next_service_entry = ttk.Entry(inputs_frame)
         self.next_service_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
@@ -56,7 +38,7 @@ class EmailGenerator:
         self.rate_entry = ttk.Entry(inputs_frame)
         self.rate_entry.grid(row=1, column=3, padx=5, pady=5, sticky="w")
 
-        ## Buttons
+        # Buttons
         # Create a frame to hold the buttons (inside the root window)
         button_frame = ttk.Frame(root)
         button_frame.pack(pady=10)  # Add some spacing above the buttons
@@ -71,14 +53,13 @@ class EmailGenerator:
         clear_button = ttk.Button(button_frame, text="Clear Form", command=lambda: clear_form(self))
         clear_button.grid(row=0, column=2, padx=5)  # Right button
 
-        # Button and inputs design
         inputs_frame.configure(style="Custom.TFrame")
         button_frame.configure(style="Custom.TFrame")
 
         style = ttk.Style()
         style.configure("Custom.TFrame", background="#49A864") 
 
-        ## Email Display
+        # Email Display
         self.email_display = tk.Text(
             root, 
             height=30, 
